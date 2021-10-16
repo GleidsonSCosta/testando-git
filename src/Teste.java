310cf9cc39a216e6ad20f1570e033189bf648f9e
Gleidson Costa <gleidsoncosta92@hotmail.com>
2021-10-16 17:43:57 -0300

    novo teste

--- src/Teste.java
+++ src/Teste.java
@@ -10,8 +10,7 @@ public class Teste {
 		/*
 		usei o comando (git clean -df)  em seguida (git checkou -- .) para voltar o código como estava
 		para trocar apenas a mensagem do último commit usei o comando (git reset --soft HEAD~1)
-		
-		
+		para cancelar o último commit (git push -f origin HEAD^:master)
 		*/
 		
 	}
