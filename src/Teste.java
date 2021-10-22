f18b50e8f22ef48b2fb7d33c1016719bc277f898
Gleidson Costa <gleidsoncosta92@hotmail.com>
2021-10-21 21:59:47 -0300

    teste

--- src/Teste.java
+++ src/Teste.java
@@ -9,7 +9,7 @@ public class Teste {
 		System.out.println("Novo dia");
 		System.out.println("Teste");
 		/*
-		usei o comando (git clean -df)  em seguida (git checkou -- .) para voltar o código como estava
+		usei o comando (git clean -df)  em seguida (git checkout -- .) para voltar o código como estava
 		para trocar apenas a mensagem do último commit usei o comando (git reset --soft HEAD~1)
 		para cancelar o último commit (git push -f origin HEAD^:master)
 		*/
