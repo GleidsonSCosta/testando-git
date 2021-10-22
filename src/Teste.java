7f9e995d21c886a03ffc9324a4bc6cedc61cc4d5
Gleidson Costa <gleidsoncosta92@hotmail.com>
2021-10-21 22:03:38 -0300

    teste

--- src/Teste.java
+++ src/Teste.java
@@ -8,6 +8,8 @@ public class Teste {
 		System.out.println("Obrigado");
 		System.out.println("Novo dia");
 		System.out.println("Teste");
+		System.out.println("Novo teste");
+		
 		/*
 		usei o comando (git clean -df)  em seguida (git checkout -- .) para voltar o código como estava
 		para trocar apenas a mensagem do último commit usei o comando (git reset --soft HEAD~1)
